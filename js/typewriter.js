7e11b35c697a10c635219a8477c7ca551abe76e5
IGiotto <101021686+IGiotto12@users.noreply.github.com>
2024-10-28 22:19:48 -0700

    clean up

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.getElementById('typewriter-text');
-    const text = "Hey, I am a first-year undergrad major in Math-CS at UC San Diego. I plan to double major in Cog-Sci specializing in ML next year. I am frequently looking for an internship/research program in ML, data analysis, or UI/UX this summer 2024. Nice to meet U";
+    const text = "Hey, I am a second-year undergrad major in Math-CS at UC San Diego. Planning to double major in Cog-Sci specializing in ML next year. I am frequently looking for an internships in SWE, ML, data analysis, or UI/UX this summer 2025. Nice to meet U";
     let index = 0;
 
     function type() {
