561a52b2c067e52c8b77fc26151d38b1619e95ee
IGiotto <101021686+IGiotto12@users.noreply.github.com>
2025-06-17 12:14:48 -0400

    .

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.getElementById('typewriter-text');
-    const text = "Hey, I am a 3rd-year undergrad major in Math-CS and minor in ICAM at UC San Diego. I am actively looking for various internship positions about Software Engineer/Machine Learning Engineer/Data Analysis/Web Development this summer 2025. Nice to meet U";
+    const text = "Hey, I am a 3rd-year undergrad major in Math-CS and minor in ICAM at UC San Diego. I am actively looking for internship positions about MLE, Data Analyst, WebDevelopment in summer 2025. Nice to meet U";
     let index = 0;
 
     function type() {
